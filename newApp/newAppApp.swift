0641b38d92c80db3351f75aad2587e4ae377402a
Lily Mosisa <lilymosisa13@gmail.com>
2025-07-08 13:34:32 -0400

    Initial Commit

---
+++ newApp/newAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppApp.swift
+//  newApp
+//
+//  Created by Lily Mosisa on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct newAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ newAppTests/newAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  newAppTests.swift
+//  newAppTests
+//
+//  Created by Lily Mosisa on 7/8/25.
+//
+
+import Testing
+@testable import newApp
+
+struct newAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
